b7a97cfc1ccc399b03c48272c5c027462d61c745
BirthavarshiniP <154209812+BirthavarshiniP@users.noreply.github.com>
2024-03-03 21:10:35 +0530

    Update summary.py

--- summary.py
+++ summary.py
@@ -14,6 +14,7 @@ import auth_functions
 # Download NLTK resources
 nltk.download('vader_lexicon')
 nltk.download('wordnet')
+nltk.download('punkt')
 
 # Load the sentiment analysis pipeline
 sia = SentimentIntensityAnalyzer()
